052b619cb20075d932c44a889b466581a1ac716d
Rizvan Karimov <karimovrizvan84@gmail.com>
2024-01-25 09:01:24 +0100

    created testing with given, when and then methoden

--- src/main/java/com/rizvankarimov/springboot/junit5mavenstarter/exemple/Calculator.java
+++ src/main/java/com/rizvankarimov/springboot/junit5mavenstarter/exemple/Calculator.java
@@ -4,4 +4,12 @@ public class Calculator {
     public int add(int count1, int count2){
         return count1 + count2;
     }
+
+    public int sub(int count1, int count2){
+        return count1-count2;
+    }
+
+    public int mul(int count1, int count2) {
+        return count1*count2;
+    }
 }
--- src/test/java/com/rizvankarimov/springboot/junit5mavenstarter/CalculatorTest.java
+++ src/test/java/com/rizvankarimov/springboot/junit5mavenstarter/CalculatorTest.java
@@ -8,16 +8,39 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CalculatorTest {
     @Test
     public void addTest() {
-
         //Ön təyinatlar
         var calculator = new Calculator();
         int count1 = 5;
         int count2 = 10;
 
         //Əməliyyat
-        int result = calculator.add(count1, count2);
+        int resultAdd = calculator.add(count1, count2);
 
         //Nəticələrin yoxlanması
-        assertEquals(15, result);
+        assertEquals(15, resultAdd);
+    }
+
+    @Test
+    public void subTest(){
+        //Ön teyinatlar
+        var calculator = new Calculator();
+        int count1 = 10;
+        int count2 = 5;
+
+        //Əməliyyat
+        int resultSub = calculator.sub(count1, count2);
+
+        //Nəticələrin yoxlanması
+        assertEquals(5, resultSub);
+
+    }
+
+    @Test
+    public void mulTest(){
+        var mulCalculator = new Calculator();
+        int count1 = 5;
+        int count2 = 10;
+        int resultMul = mulCalculator.mul(count1, count2);
+        assertEquals(50,resultMul);
     }
 }
